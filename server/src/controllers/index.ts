4b20711fd96f07b6063ec074e34b67d77cb2914c
Eric Littlejohn <el@mail.fantasyportal.com>
2022-08-18 14:13:52 -0400

    push azure function as server

--- server/src/controllers/index.ts
+++
@@ -1,1 +0,0 @@
-export { PlayersController } from './players';
--- server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { PlayersController } from '../controllers';
-
-export function registerRoutes(router: Router) {
-    /* Players api */
-    router.get('/api/players', PlayersController.getPlayers)
-}
